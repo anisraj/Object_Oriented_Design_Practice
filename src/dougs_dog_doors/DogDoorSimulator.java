6acaea741ff164cb9ed717aebaf19339e3db5f5c
Anis Jamadar <anisjamadar26@gmail.com>
2023-01-14 21:47:06 +0530

    door code

--- src/dougs_dog_doors/DogDoorSimulator.java
+++ src/dougs_dog_doors/DogDoorSimulator.java
@@ -4,13 +4,26 @@ public class DogDoorSimulator {
     public static void main() {
         DogDoor dogDoor = new DogDoor();
         Remote remote = new Remote(dogDoor);
+
         System.out.println("Fido barks to go outside...");
         remote.pressButton();
         System.out.println("\nFido has gone outside...");
-        remote.pressButton();
+
         System.out.println("\nFido is all done...");
+
+        try {
+            Thread.currentThread().sleep(1000);
+        } catch (InterruptedException e) { }
+
+        System.out.println("but he is stuck outside...");
+
+        System.out.println("\nFido starts barking...");
+
+        System.out.println("...so Gine grabs the remote control.");
+
         remote.pressButton();
+
         System.out.println("\nFido is back inside...");
-        remote.pressButton();
+
     }
 }
