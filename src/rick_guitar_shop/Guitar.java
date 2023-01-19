f56866b1e7c0573bce735c608707a72206ac99c3
Anis Jamadar <anisjamadar26@gmail.com>
2023-01-19 08:50:24 +0530

    good design

--- src/rick_guitar_shop/Guitar.java
+++ src/rick_guitar_shop/Guitar.java
@@ -1,26 +1,10 @@
 package rick_guitar_shop;
 
-public class Guitar {
-    private String serialNumber;
-    private double price;
-    private GuitarSpec guitarSpec;
+public class Guitar extends Instrument {
 
     public Guitar(String serialNumber, double price,
                   GuitarSpec guitarSpec) {
-        this.serialNumber = serialNumber;
-        this.price = price;
-        this.guitarSpec = guitarSpec;
+        super(serialNumber, price, guitarSpec);
     }
 
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public GuitarSpec getGuitarSpec() {
-        return guitarSpec;
-    }
 }
---
+++ src/rick_guitar_shop/Mandolin.java
@@ -0,0 +1,8 @@
+package rick_guitar_shop;
+
+public class Mandolin extends Instrument {
+
+    public Mandolin(String serialNumber, double price, MandolinSpec mandolinSpec) {
+        super(serialNumber, price, mandolinSpec);
+    }
+}
